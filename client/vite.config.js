280382e137aaf928bdcee0b36fa0672776b81817
JustFrogg <weekiftxvgx@gmail.com>
2025-01-27 22:06:43 +0300

    Первоначальный коммит

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,5 +15,6 @@ export default defineConfig({
     hmr: {
       clientPort: 443,
     },
+    allowedHosts: ['southampton-asking-need-commission.trycloudflare.com', 'localhost', '127.0.0.1'],
   },
 });
